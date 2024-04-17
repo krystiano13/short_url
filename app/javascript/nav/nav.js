fd14d96fb4274c3a3d89321999337b4363a2a2f5
krystiano13 <crystianotv@gmail.com>
2024-04-17 17:33:04 +0200

    create mobile version of navbar

--- app/javascript/nav/nav.js
+++ app/javascript/nav/nav.js
@@ -1,5 +1,9 @@
 const hamburger_button = document.querySelector("#hamburger_button");
+const nav_menu = document.querySelector("#nav_menu");
+
 function toggle_navbar() {
     hamburger_button.classList.toggle("bg-purple-custom1");
+    nav_menu.classList.toggle("translate-y-[-100vh]");
 }
+
 hamburger_button.addEventListener('click', () => toggle_navbar());
